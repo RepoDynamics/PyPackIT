3efa6d0121935d398e9a62fd9d7bccb8280ca2aa
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-07-04 00:35:13 +0200

    Create sync_files.py

---
+++ dev/repo_sync/sync_files.py
@@ -0,0 +1,20 @@
+
+from pathlib import Path
+import argparse
+import json
+
+
+def main():
+    with open("./data/repo_templates/CONTRIBUTING.md") as f:
+        text = f.read()
+    with open("./docs/CONTRIBUTING.md", "w") as f:
+        f.write(text.format(project_name="PyPackIT"))
+    return
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("filepath")
+    # args = parser.parse_args()
+    # main(args.filepath)
+    main()
