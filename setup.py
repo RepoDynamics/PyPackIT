ae96591a82ead7a4a22d84203c6f036a50e72ec6
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-07-27 13:29:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,44 @@
+from typing import Any
+import re
+
 from setuptools import setup
-from versioningit import get_cmdclasses
+import versioningit
 
 
-setup(cmdclass=get_cmdclasses())
+def versioningit_format_custom(
+    *,
+    description: versioningit.VCSDescription,
+    base_version: str,
+    next_version: str,
+    params: dict[str, Any],
+) -> str:
+    branch: str | None = description.branch
+    if not branch:
+        issue_nr = "unknown"
+    elif branch == params['default-branch']:
+        issue_nr = "0"
+    else:
+        match = re.match(r"^dev/(\d+)$", branch)
+        issue_nr = match.group(1) if match else branch
+
+    fields = {
+        **description.fields,
+        "issue_nr": issue_nr,
+        "base_version": base_version,
+        "next_version": next_version,
+    }
+    formats = {
+        "distance": "{base_version}.{issue_nr}.dev{distance}",
+        "dirty": "base_version}.{issue_nr}.dev{distance}.{author_date:%Y.%m.%d}.{rev}.dirty",
+        "distance-dirty": "base_version}.{issue_nr}.dev{distance}.{author_date:%Y.%m.%d}.{rev}.dirty",
+    }
+    if description.state not in formats:
+        raise versioningit.ConfigError(
+            f"No format string found for state {description.state!r}."
+        )
+    fmt = formats[description.state]
+    return fmt.format_map(fields)
+
+
+if __name__ == '__main__':
+    setup(cmdclass=versioningit.get_cmdclasses())
