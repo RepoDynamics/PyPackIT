5779697a6196e9a3b15bcb1278a903a6f1cbbb9b
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-08-02 17:36:23 +0200

    update

--- .github/scripts/changed_files.py
+++ .github/scripts/changed_files.py
@@ -1,20 +1,28 @@
+"""
+Parse outputs from `actions/changed-files` action.
+
+This script is used in the `repo_changed_files.yaml` workflow.
+It parses the outputs from the `actions/changed-files` action and
+creates a new output variable `json` that contains all the data,
+and writes a job summary.
+"""
+
 import os
 import json
 
-# with open(os.environ["GROUPS"]) as f:
-#     groups = json.load(f)
-# with open(os.environ["ALL"]) as f:
-#     all_files = json.load(f)
 
+# Get actions outputs from environment variables (set in workflow)
 groups = json.loads(os.environ["GROUPS"], strict=False)
 all_files = dict(sorted(json.loads(os.environ["ALL"], strict=False).items()))
+
+# Parse and clean outputs
 sep_groups = dict()
+group_summary_str = ""
 
 for item_name, val in groups.items():
     group_name, attr = item_name.split("_", 1)
     group = sep_groups.setdefault(group_name, dict())
     group[attr] = val
-group_summary_str = ""
 
 for group_name, group_attrs in sep_groups.items():
     sep_groups[group_name] = dict(sorted(group_attrs.items()))
@@ -25,9 +33,11 @@ for group_name, group_attrs in sep_groups.items():
 all_groups = {"all": all_files} | sep_groups
 file_list = "\n".join(sorted(all_files["all_changed_and_modified_files"].split()))
 
+# Create output variable `json`
 with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
     print(f"json={json.dumps(all_groups)}", file=fh)
 
+# Write job summary
 summary = f"""#### Modified Categories
 {group_summary_str}
 
@@ -47,6 +57,5 @@ summary = f"""#### Modified Categories
 
 </details>
 """
-
 with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as fh:
     print(summary, file=fh)
