68ace855d9cd2f3f8cfcff6a3fd953b1e7dd7e5d
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-11-13 10:36:01 +0100

    Update general_tests.py

--- tests/src/pypackit_tests/general_tests.py
+++ tests/src/pypackit_tests/general_tests.py
@@ -1,8 +1,12 @@
-# Self
+"""General tests"""  # noqa: D400
+
+
 import pypackit as pkg
 
 
 def version_test():
     """Sample test."""
+    min_len_ver_elements = 3
     assert isinstance(pkg.__version__, str)
-    assert len(pkg.__version__.split(".")) >= 3
+    assert len(pkg.__version__.split(".")) >= min_len_ver_elements
+    return
