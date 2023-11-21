1481b6169874707b499fb9734f1c0cd86c4e6b30
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-11-21 23:03:27 +0100

    Update generator.py

--- .meta/custom/generator.py
+++ .meta/custom/generator.py
@@ -25,21 +25,32 @@ def extract_defaults_from_schema(schema: str) -> tuple[str, bool]:
 
 
 def schemas():
-    path = files("repodynamics") / "_data" / "schema"
-    filepaths = list(path.glob("**/*.yaml"))
+    path_data = files("repodynamics") / "_data"
+    path_schema = path_data / "schema"
+    path_examples = path_data / "example"
+    filepaths = list(path_schema.glob("**/*.yaml"))
     out = {}
     for filepath in filepaths:
         schema = filepath.read_text()
         defaults, all_have_defaults = extract_defaults_from_schema(schema)
-        entry = {"full": schema, "defaults": defaults, "all_have_defaults": all_have_defaults}
-        rel_path = filepath.relative_to(path)
-        name = rel_path.stem
-        if rel_path.parent == Path("."):
-            out[name] = entry
-        else:
-            sub_dict = out.setdefault(rel_path.parent.name, {})
-            sub_dict[name] = entry
-    return {"schema": out}
+        rel_path = filepath.relative_to(path_schema)
+        path_example = (path_examples / rel_path)
+
+        entry = {
+            "schema_str": schema,
+            "default_str": defaults,
+            "example_str": path_example.read_text() if path_example.exists() else "",
+            "pre_config": all_have_defaults,
+            "path": str(rel_path)
+        }
+        out[f"manual/meta/{rel_path.with_suffix('')}"] = entry
+        # name = rel_path.stem
+        # if rel_path.parent == Path("."):
+        #     out[name] = entry
+        # else:
+        #     sub_dict = out.setdefault(rel_path.parent.name, {})
+        #     sub_dict[name] = entry
+    return {"meta": out}
 
 
 def run(metadata) -> dict:
