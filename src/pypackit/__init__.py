b84d8da5a3bc45528413afabc437bc4a197cb946
AAriam <80158628+AAriam@users.noreply.github.com>
2024-01-05 10:10:32 +0000

    auto-update: Sync dynamic files

--- src/pypackit/__init__.py
+++ src/pypackit/__init__.py
@@ -21,27 +21,27 @@ of the development pipeline, PyPackIT fuels innovation and growth in the Python 
 enabling aspiring and experienced developers alike to rapidly build professional software, readily
 share them with the world, and effortlessly maintain their projects. With PyPackIT, you are free to
 solely focus on what truly matters: implementing your ideas and bringing your vision to life!
-Copyright 2023 RepoDynamics
+Copyright 2023–2024 RepoDynamics
 
-Copyright (C) 2023 RepoDynamics
+Copyright (C) 2023–2024 RepoDynamics
 
-This program is free software: you can
-redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published
+This program is free
+software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public
+License as published
 by the Free Software Foundation, either version 3 of the License, or
-(at your option) any
-later version.
+(at your
+option) any later version.
 
 This program is distributed in the hope that it will be useful,
-but WITHOUT ANY
-WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the
+but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
-You should have received a copy of the
-GNU Affero General Public License
+You should have
+received a copy of the GNU Affero General Public License
 along with this program.  If not, see
 <https://www.gnu.org/licenses/>.
 """  # noqa: D400
