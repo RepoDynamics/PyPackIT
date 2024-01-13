53b9cea033d3ccb340f76c0ea6e09d292591b576
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-13 13:27:51 +0100

    Update conf.py

--- docs/website_template/source/conf.py
+++ docs/website_template/source/conf.py
@@ -364,7 +364,7 @@ html_permalinks_icon: str = "¶"
 
 html_sidebars: Dict[str, Union[List[str], str]] = {
     # "**": ["sidebar-nav-bs"],
-    "news/**": [
+    f'{meta["web"]["path"]["news"]}/**': [
         "ablog/postcard.html",
         "ablog/recentposts.html",
         "ablog/tagcloud.html",
@@ -626,8 +626,8 @@ blog_path: str = meta["web"]["path"]["news"]
 blog_baseurl: str = meta["url"]["website"]["base"]
 
 blog_post_pattern: list[str] = [
-    f"{blog_path}/posts/*.rst",
-    f"{blog_path}/posts/*.md",
+    f"{blog_path}/post/*.rst",
+    f"{blog_path}/post/*.md",
 ]
 post_auto_image: int = 1
 blog_feed_archives: bool = True
