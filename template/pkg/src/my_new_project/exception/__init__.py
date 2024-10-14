5f63e932e9c53a0f0b4275d048f5f586d820a870
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-10-14 11:54:49 +0200

    Update __init__.py

--- template/pkg/src/my_new_project/exception/__init__.py
+++ template/pkg/src/my_new_project/exception/__init__.py
@@ -1,5 +1,5 @@
 """Exceptions raised by the package."""
 
 
-class PackageException(Exception):  # noqa: N818
+class PackageError(Exception):
     """Base class for all exceptions raised by this package."""
