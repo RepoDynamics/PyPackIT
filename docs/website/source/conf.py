15498f9bcd3b788530b0a2649a02eb0333655ea5
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-20 00:15:38 +0100

    Update conf.py

--- docs/website/source/conf.py
+++ docs/website/source/conf.py
@@ -5,9 +5,9 @@ from __future__ import annotations as _annotations
 import ast as _ast
 import copy as _copy
 import json as _json
+import os as _os
 from pathlib import Path as _Path
 from typing import TYPE_CHECKING as _TYPE_CHECKING
-import os as _os
 
 import gittidy as _git
 from loggerman import logger as _logger
@@ -25,14 +25,14 @@ if _TYPE_CHECKING:
     from sphinx.application import Sphinx
 
 
-_METADATA_FILEPATH: str = ".github/.control/.metadata.json"
+_METADATA_FILEPATH: str = ".github/.repodynamics/metadata.json"
 _globals: dict = {}
 
 
 class _CustomDirectoryHTMLBuilder(_DirectoryHTMLBuilder):
     """Customized DirectoryHTMLBuilder to exclude the 404 file."""
 
-    name = 'dirhtml'
+    name = "dirhtml"
 
     def get_target_uri(self, docname: str, typ: str | None = None) -> str:
         if docname == "404":
@@ -41,7 +41,7 @@ class _CustomDirectoryHTMLBuilder(_DirectoryHTMLBuilder):
 
     def get_outfilename(self, pagename: str) -> str:
         if pagename == "404":
-            return _os.path.join(self.outdir, '404.html')
+            return _os.path.join(self.outdir, "404.html")
         return super().get_outfilename(pagename=pagename)
 
 
@@ -80,22 +80,22 @@ def linkcode_resolve(domain: str, info: dict[str, str]) -> str | None:
         tree = _ast.parse(source, filename=filepath)
         for node in _ast.walk(tree):
             # Check for class or function definitions
-            if isinstance(
-                node,
-                (_ast.ClassDef, _ast.FunctionDef, _ast.AsyncFunctionDef)
-            ) and node.name == object_name:
-                return node.lineno, getattr(node, 'end_lineno', None)
+            if (
+                isinstance(node, (_ast.ClassDef, _ast.FunctionDef, _ast.AsyncFunctionDef))
+                and node.name == object_name
+            ):
+                return node.lineno, getattr(node, "end_lineno", None)
             # Check for variable assignments (without type annotations)
             if isinstance(node, _ast.Assign):
                 for target in node.targets:
                     if isinstance(target, _ast.Name) and target.id == object_name:
-                        return node.lineno, getattr(node, 'end_lineno', None)
+                        return node.lineno, getattr(node, "end_lineno", None)
             # Check for variable assignments (with type annotations)
             if isinstance(node, _ast.AnnAssign):
                 target = node.target
                 if isinstance(target, _ast.Name) and target.id == object_name:
-                    return node.lineno, getattr(node, 'end_lineno', None)
-        return
+                    return node.lineno, getattr(node, "end_lineno", None)
+        return None
 
     def add_obj_line_number_to_url(
         url: str,
@@ -117,13 +117,15 @@ def linkcode_resolve(domain: str, info: dict[str, str]) -> str | None:
         -------
         URL to the source file with added line numbers of the object definition, if found.
         """
-        log_intro = f"Resolved source-code filepath of module `{info["module"]}` to `{module_path_abs}`"
+        log_intro = (
+            f"Resolved source-code filepath of module `{info["module"]}` to `{module_path_abs}`"
+        )
         lines = get_obj_def_lines(filepath=filepath, object_name=object_name)
         if not lines:
             _logger.warning(
                 logger_title,
                 f"{log_intro}, but could not find object `{object_name}` in the file.",
-                f"Generated URL (without line numbers): {url}"
+                f"Generated URL (without line numbers): {url}",
             )
             return url
         start_line, end_line = lines
@@ -137,20 +139,20 @@ def linkcode_resolve(domain: str, info: dict[str, str]) -> str | None:
         _logger.success(
             logger_title,
             f"{log_intro} and found object `{object_name}` at {log_segment}.",
-            f"Generated URL: {final_url}"
+            f"Generated URL: {final_url}",
         )
         return final_url
 
     logger_title = "LinkCode Resolve"
-    if domain != 'py' or not info['module']:
+    if domain != "py" or not info["module"]:
         _logger.warning(
             logger_title,
             "Invalid domain or module information:",
             _logger.pretty({"domain": domain, "info": info}),
         )
-        return
-    source_path = _Path(f"{_meta["pkg"]["path"]["root"]}/{_meta["pkg"]["path"]["source"]}")
-    module_path = source_path / info['module'].replace('.', '/')
+        return None
+    source_path = _Path(_meta["pkg"]["path"]["source"])
+    module_path = source_path / info["module"].replace(".", "/")
     module_path_abs = _path_root / module_path
     if module_path_abs.is_dir() and module_path_abs.joinpath("__init__.py").is_file():
         filepath = module_path.joinpath("__init__.py")
@@ -161,9 +163,11 @@ def linkcode_resolve(domain: str, info: dict[str, str]) -> str | None:
             logger_title,
             f"Python module {info["module"]} not found at {module_path_abs}.",
         )
-        return
+        return None
     url = f"https://github.dev/{_meta["repo"]["full_name"]}/blob/{_current_hash}/{filepath}"
-    return add_obj_line_number_to_url(url=url, filepath=_path_root/filepath, object_name=info["fullname"])
+    return add_obj_line_number_to_url(
+        url=url, filepath=_path_root / filepath, object_name=info["fullname"]
+    )
 
 
 def _source_jinja_template(app: Sphinx, docname: str, content: list[str]) -> None:
