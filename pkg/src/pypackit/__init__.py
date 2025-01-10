7ecf3522ed2d0ea5c04d9fa8e99485e834e982c6
AAriam <80158628+AAriam@users.noreply.github.com>
2025-01-10 06:13:06 +0100

    Update __init__.py

--- pkg/src/pypackit/__init__.py
+++ pkg/src/pypackit/__init__.py
@@ -7,25 +7,26 @@ PyPackIT [ˈpaɪˌpækɪt] is a comprehensive <strong>cloud-based automation too
 of <abbr title="Findable, Accessible, Interoperable, and Reusable">FAIR</abbr> and professional
 software on <strong>GitHub</strong>, in accordance with the latest engineering best practices and
 standards. It is a ready-to-use software suite that streamlines the initiation, configuration,
-development, publication, management, and maintenance of high-quality software libraries in
-<strong>Python</strong>. By taking charge of repetitive tasks and automatically enforcing best
-practices throughout the software development life-cycle, PyPackIT enables users to solely focus on
+development, publication, management, and maintenance of high-quality <strong>Python
+applications</strong>. By taking charge of repetitive tasks and automatically enforcing best
+practices throughout the software development life cycle, PyPackIT enables users to solely focus on
 the creative aspects of their projects, while improving quality and lowering production costs. Using
-state-of-the-art tools and methodologies, PyPackIT offers a robust project infrastructure, including
-a build-ready Python <strong>package skeleton</strong>, a fully operational <strong>test
-suite</strong>, a complete <strong>documentation website</strong>, and a comprehensive
-<strong>control center</strong> facilitating Continuous Configuration Automation (CCA) and dynamic
-project management. PyPackIT establishes a complete <strong>cloud-native development</strong>
-environment on GitHub, integrating with its version control system, issue tracker, and pull-based
-model to provide a fully automated software development workflow, complete with features like
-<strong>issue management</strong>, <strong>branching model</strong>, and <strong>version
-scheme</strong>. Leveraging <strong>GitHub Actions</strong> (GHA), PyPackIT implements a cloud-
-native development process with specialized Continuous Integration (CI), Deployment (CD), Testing
-(CT), Refactoring (CR), and Maintenance (CM) pipelines, using <strong>Agile</strong>,
-<strong>Continuous software engineering</strong>, and <strong>DevOps</strong> methodologies.
-PyPackIT is a free and open-source software suite that readily integrates with both new and existing
-projects to ensure their long-term sustainability and high quality, thus enabling developers to
-rapidly implement their ideas and easily maintain their software.
+latest tools and methodologies, PyPackIT offers a robust project infrastructure, including a build-
+ready Python <strong>package skeleton</strong>, a fully operational <strong>test suite</strong>, an
+automated <strong>documentation website</strong>, and a comprehensive <strong>control
+center</strong> according to <strong>Infrastructure-as-Code</strong> and <strong>Continuous
+Configuration Automation</strong> practices to enable dynamic project management and customization.
+PyPackIT establishes a complete <strong>cloud-native development</strong> environment on GitHub,
+integrating with its version control system, issue tracker, and pull-based model to provide a fully
+<strong>automated software development workflow</strong>, complete with features like <strong>issue
+management</strong>, <strong>branching model</strong>, and <strong>version scheme</strong>.
+Leveraging <strong>GitHub Actions</strong>, PyPackIT implements a cloud-native development process
+with specialized <strong>Continuous Integration</strong>, Deployment, Testing, Refactoring, and
+Maintenance pipelines using <strong>Agile</strong>, <strong>Continuous software
+engineering</strong>, and <strong>DevOps</strong> methodologies. PyPackIT is a <strong>free</strong>
+and <strong>open-source</strong> software suite that readily integrates with both new and existing
+projects to ensure their long-term sustainability and high quality, enabling developers to rapidly
+implement their ideas and easily maintain their software.
 """  # noqa: RUF002
 
 __all__ = ["__version_details__", "__version__"]
