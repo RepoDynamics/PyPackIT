7ee1c5cb4d988c40808718972442f496d3cdf6bb
AAriam <80158628+AAriam@users.noreply.github.com>
2025-03-04 01:04:24 +0100

    Create run.py

---
+++ .dev/run.py
@@ -0,0 +1,37 @@
+from __future__ import annotations as _annotations
+
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+import json
+from pathlib import Path as _Path
+
+
+class DevRunner:
+    def __init__(self, metadata_filepath: _Path | str = ".github/.repodynamics/metadata.json"):
+        self.data = json.loads(_Path(metadata_filepath).read_text())
+        return
+
+    @property
+    def environments(self) -> list[str]:
+        return [key[4:] for key in self.data if key.startswith("env_")]
+
+    def run(self, environment: str, feature: str, task: str):
+        env = self.data.get(f"env_{environment}")
+        if not env:
+            raise ValueError(
+                f"Environment '{environment}' not found in metadata. "
+                f"Available environments are: {", ".join(self.environments)}"
+            )
+        feature = env.get(feature)
+        if not feature:
+            raise ValueError(
+                f"Feature '{feature}' not defined for environment '{environment}'. "
+                f"Available features are: {", ".join(env.keys())}"
+            )
+        task = feature.get(task)
+        if not task:
+            raise ValueError(
+                f"Task '{task}' not defined for feature '{feature}' in environment '{environment}'. "
+                f"Available tasks are: {", ".join(feature.keys())}"
+            )
+
