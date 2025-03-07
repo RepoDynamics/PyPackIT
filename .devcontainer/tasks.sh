aeb857d335accd6964d973db619daacc86a438e3
AAriam <80158628+AAriam@users.noreply.github.com>
2025-03-07 02:10:47 +0100

    Update tasks.sh

--- .devcontainer/tasks.sh
+++ .devcontainer/tasks.sh
@@ -1,3 +1,7 @@
+jupyterlab() {
+    jupyter-lab / --ContentsManager.allow_hidden=True --IdentityProvider.token=''
+}
+
 build-oj-paper() {
     docker exec -it openjournals \
       inara -o pdf -p -v $1
