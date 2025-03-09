c362a060a4795c2ba562986dd50e9e83f6f75580
AAriam <80158628+AAriam@users.noreply.github.com>
2025-03-09 23:47:07 +0100

    Update cca_inline.py

--- .control/hooks/cca_inline.py
+++ .control/hooks/cca_inline.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 import importlib.util as _importlib_util
 import sys as _sys
 from pathlib import Path as _Path
+import json as _json
+import shlex as _shlex
 from typing import TYPE_CHECKING
 
 import mdit
@@ -97,6 +99,91 @@ class Hooks:
         self.get = get_metadata
         self.changelog(get_metadata=get_metadata)
         return self
+
+    def pypkg_test(self, test_pkg_id: str) -> dict:
+        """Create test package data."""
+
+        def install_env_script(
+            build_platform: str,
+            python_version: str,
+            conda_env_name: str,
+            source: Literal["pip", "conda"],
+        ):
+            sources = ["pip", "conda"] if source == "pip" else ["conda", "pip"]
+            if build_platform.startswith("linux"):
+                sources.extend(["apt", "bash", "brew"])
+            elif build_platform.startswith("osx"):
+                sources.extend(["brew", "bash"])
+            elif build_platform.startswith("win"):
+                sources.extend(["choco", "pwsh"])
+            return (
+                f"python ./.devcontainer/install.py "
+                f"--packages {packages} "
+                f"--build-platform {build_platform} "
+                f"--sources {' '.join(sources)} "
+                f"--python-version {python_version} "
+                f"--conda-env-name {conda_env_name} "
+                "--exclude-install conda "
+                f"--output-dir {env_output_dir} "
+                "--overwrite "
+                f"--filename-conda {conda_filename} "
+            )
+
+        def install_pkg_script(
+            source: Literal["pip", "conda"],
+        ):
+            if source == "conda":
+                return (
+                    f"conda install -c file://./ {package_names_str}"
+                )
+            return (
+                f"pip install --no-index --find-links=./ --prefer-binary {package_names_str}"
+            )
+
+        env_output_dir = "./_temp_test_env"
+        conda_filename = "environment.yaml"
+        pkg_id = self.get(".__key__").removeprefix("pypkg_")
+        packages = _shlex.quote(_json.dumps([pkg_id, test_pkg_id]))
+        package_names = [
+            self.get(".name").lower(),
+            self.get(f"pypkg_{test_pkg_id}.name").lower()
+        ]
+        package_names_str = ' '.join(package_names)
+        oss = self.get(".os")
+        python = self.get(".python")
+        out = {}
+        for os in oss.values():
+            for source in ("pip", "conda"):
+                for python_version in python["version"]["minors"]:
+                    conda_env_name = f"{pkg_id}-{os["platform"]}-{os["runner"]}-{source}-py{python_version}"
+                    out[f"{os['platform']}_{os['runner']}_{source}_py{python_version}"] = {
+                        "name": f"py{python_version} | {source} | {os['name']}",
+                        "runner": os["runner"],
+                        "script": {
+                            "install_env": install_env_script(
+                                build_platform=os["platform"],
+                                python_version=python_version,
+                                conda_env_name=conda_env_name,
+                                source=source,
+                            ),
+                            "install_pkg": install_pkg_script(source=source),
+                            "test": f"python -m {package_names[1]} --report ./report"
+                        },
+                        "conda_env": {
+                            "name": conda_env_name,
+                            "path": f"{env_output_dir}/{conda_filename}",
+                        },
+                        "codecov": {
+                            "env": {
+                                "PLATFORM": os["platform"],
+                                "RUNNER": os["runner"],
+                                "PYTHON": python_version,
+                                "PKG_MANAGER": source,
+                            }
+                        },
+                        "report_artifact_suffix": f" - {source} - {os['runner']} - {os['platform']} - py{python_version}",
+                    }
+        return out
 
     def binder_config_file(self, source: Literal["conda", "apt", "bash"]) -> str:
         """Create environment dependencies for binder."""
