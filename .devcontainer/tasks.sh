7a53bc82920c43e7349d5bdd633414ac5878bc16
AAriam <80158628+AAriam@users.noreply.github.com>
2025-03-13 17:27:38 +0100

    remove old dynamic files

--- .devcontainer/tasks.sh
+++
@@ -1,8 +0,0 @@
-jupyterlab() {
-    jupyter-lab / --ContentsManager.allow_hidden=True --IdentityProvider.token=''
-}
-
-build-oj-paper() {
-    docker exec -it openjournals \
-      inara -o pdf -p -v $1
-}
