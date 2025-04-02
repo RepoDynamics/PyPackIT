840022b0c71a3ad123980d2efc3846ec712b2f5d
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-02 23:49:50 +0200

    sync dynamic files

--- .devcontainer/tasks_global.sh
+++ .devcontainer/tasks_global.sh
@@ -35,14 +35,6 @@ typecheck() {
     conda run --name type_check --live-stream -vv mypy '--package pypackit' '--package pypackit_testsuite' '--python-executable /opt/conda/envs/{'"'"'type'"'"': '"'"'string'"'"', '"'"'description'"'"': '"'"'Name of the conda environment to use for installation.'"'"', '"'"'default'"'"': '"'"'app'"'"'}/bin/python' '--config-file .devcontainer/config/mypy.toml' --install-types --non-interactive
 }
 
-lint() {
-    conda run --name pre_commit --live-stream -vv python .devcontainer/script/pre_commit.py --config .devcontainer/config/pre-commit.yaml "$@"
-}
-
-lint-ci() {
-    conda run --name pre_commit --live-stream -vv python .devcontainer/script/pre_commit.py --config .devcontainer/config/pre-commit-ci.yaml "$@"
-}
-
 project() {
     conda run --name pypackit --live-stream -vv project "$@"
 }
--- .devcontainer/tasks_local.sh
+++ .devcontainer/tasks_local.sh
@@ -47,14 +47,6 @@ typecheck() {
     conda run --cwd /workspace --name type_check --live-stream -vv mypy '--package pypackit' '--package pypackit_testsuite' '--python-executable /opt/conda/envs/{'"'"'type'"'"': '"'"'string'"'"', '"'"'description'"'"': '"'"'Name of the conda environment to use for installation.'"'"', '"'"'default'"'"': '"'"'app'"'"'}/bin/python' '--config-file .devcontainer/config/mypy.toml' --install-types --non-interactive
 }
 
-lint() {
-    conda run --cwd /workspace --name pre_commit --live-stream -vv python .devcontainer/script/pre_commit.py --config .devcontainer/config/pre-commit.yaml "$@"
-}
-
-lint-ci() {
-    conda run --cwd /workspace --name pre_commit --live-stream -vv python .devcontainer/script/pre_commit.py --config .devcontainer/config/pre-commit-ci.yaml "$@"
-}
-
 project() {
     conda run --cwd /workspace --name pypackit --live-stream -vv project "$@"
 }
