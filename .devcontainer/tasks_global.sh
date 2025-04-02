fbba266d03a72abdb600dff8abc39f1d60aaf707
AAriam <80158628+AAriam@users.noreply.github.com>
2025-04-02 00:49:11 +0000

    sync(config): Synchronize dynamic files with control center configurations.
    [skip ci]

--- .devcontainer/tasks_global.sh
+++ .devcontainer/tasks_global.sh
@@ -43,6 +43,6 @@ lint-ci() {
     conda run --name pre_commit --live-stream -vv python .devcontainer/script/pre_commit.py --config .devcontainer/config/pre-commit-ci.yaml "$@"
 }
 
-cca() {
-    conda run --name pypackit --live-stream -vv cca "$@"
+project() {
+    conda run --name pypackit --live-stream -vv project "$@"
 }
--- .devcontainer/tasks_local.sh
+++ .devcontainer/tasks_local.sh
@@ -55,6 +55,6 @@ lint-ci() {
     conda run --cwd /workspace --name pre_commit --live-stream -vv python .devcontainer/script/pre_commit.py --config .devcontainer/config/pre-commit-ci.yaml "$@"
 }
 
-cca() {
-    conda run --cwd /workspace --name pypackit --live-stream -vv cca "$@"
+project() {
+    conda run --cwd /workspace --name pypackit --live-stream -vv project "$@"
 }
