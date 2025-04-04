a22200492d158ca9acf174c5601bae85f8186ff0
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-04 19:43:38 +0200

    Update report.py

--- .manager/src/proman/report.py
+++ .manager/src/proman/report.py
@@ -238,7 +238,6 @@ def initialize_logger(
 ):
     logger.initialize(
         realtime_levels=list(range(1, 7)),
-        github=True,
         github_debug=True,
         title_number=title_number,
         level_style_debug=style.log_level(
