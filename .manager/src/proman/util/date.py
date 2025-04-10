0c4cac25c3e8bb34cca6da82bae5411bc96e136d
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-10 10:13:00 +0200

    Update date.py

--- .manager/src/proman/util/date.py
+++ .manager/src/proman/util/date.py
@@ -1,6 +1,7 @@
 import datetime as _dt
 
 OUTPUT_FORMAT = "%Y-%m-%d"
+OUTPUT_FORMAT_FULL = "%Y-%m-%d--%H-%M-%S"
 
 
 def from_now() -> _dt.datetime:
@@ -8,7 +9,12 @@ def from_now() -> _dt.datetime:
 
 
 def from_internal(date: str) -> _dt.datetime:
-    return _dt.datetime.strptime(date, OUTPUT_FORMAT).astimezone(_dt.UTC)
+    for fmt in [OUTPUT_FORMAT, OUTPUT_FORMAT_FULL]:
+        try:
+            return _dt.datetime.strptime(date, fmt).astimezone(_dt.UTC)
+        except ValueError:
+            continue
+    raise ValueError(f"Input '{date}' does not match supported formats.")
 
 
 def from_iso(date: str) -> _dt.datetime:
@@ -19,12 +25,8 @@ def from_github(date: str) -> _dt.datetime:
     return _dt.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ").astimezone(_dt.UTC)
 
 
-def to_internal(date: _dt.datetime) -> str:
-    return date.astimezone(_dt.UTC).strftime(OUTPUT_FORMAT)
-
-
-def to_iso_8601(date: _dt.datetime) -> str:
-    return date.strftime("%Y-%m-%d")
+def to_internal(date: _dt.datetime, time: bool = False) -> str:
+    return date.astimezone(_dt.UTC).strftime(OUTPUT_FORMAT_FULL if time else OUTPUT_FORMAT)
 
 
 def to_iso(date: _dt.datetime) -> str:
@@ -37,3 +39,7 @@ def to_posix(date: _dt.datetime) -> int:
 
 def from_github_to_internal(date: str) -> str:
     return to_internal(from_github(date))
+
+
+def from_now_to_internal(time: bool = False) -> str:
+    return to_internal(date=from_now(), time=time)
