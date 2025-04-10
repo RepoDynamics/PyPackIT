f45e999af567566dc4ebe067f6c82d7495efd429
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-10 05:34:40 +0200

    Update version.py

--- .manager/src/proman/script/version.py
+++ .manager/src/proman/script/version.py
@@ -1,40 +1,38 @@
 """Get current project version."""
 
-import argparse
-import json
-import re
-from pathlib import Path
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
-import ansi_sgr as sgr
-import mdit
-import pyshellman as _pyshellman
-from gittidy import Git
 from loggerman import logger
+import pyshellman
 
 if TYPE_CHECKING:
-    from typing import Literal
+    from proman.manager import Manager
+
 
 _CMD_PREFIX = ["conda", "run", "--name", "versioning", "--live-stream", "-vv"]
 
 
-def run(metadata: dict, repo: str = "./") -> str:
-    """Get the current project version."""
+def run(manager: Manager) -> str:
+    """Get current project version."""
     cmd = [
-        metadata["pypkg_main"]["dependency"]["build"]["versioning"]["import_name"],
-        str(Path(repo).resolve() / metadata["pypkg_main"]["path"]["root"]),
+        manager.data["pypkg_main.dependency.build.versioning.import_name"],
+        str(manager.git.repo_path / manager.data["pypkg_main.path.root"]),
         "--verbose"
     ]
-    version = _pyshellman.run(
+    version = pyshellman.run(
         command=[
             *_CMD_PREFIX, *cmd
         ],
         text_output=True,
+        logger=logger,
     )
     return version.out
 
 
-def run_cli(args: argparse.Namespace) -> int:
-    version = run(metadata=args.metadata, repo=args.repo)
+def run_cli(args: dict) -> None:
+    """Run from CLI."""
+    version = run(manager=args["manager"])
     print(version)
-    return 0
+    return
