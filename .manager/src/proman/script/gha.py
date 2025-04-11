39a47952db62a03e987d8e706379d97d1e10a4d6
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-11 06:21:54 +0200

    Update gha.py

--- .manager/src/proman/script/gha.py
+++ .manager/src/proman/script/gha.py
@@ -1,96 +1,39 @@
 from __future__ import annotations
 
-from pathlib import Path
 from typing import TYPE_CHECKING
 
-import actionman as _actionman
-import github_contexts as _github_contexts
+import actionman
+
 import mdit
 import pyserials as ps
-from github_contexts.github.enum import EventType as _EventType
+from github_contexts.github.enum import EventType
 from loggerman import logger
-from rich.text import Text
 
-from proman import event_handler as _handler
-from proman import exception as _exception
-from proman.dstruct import Token
-from proman.output import OutputManager
-from proman.report import Reporter, make_sphinx_target_config
+
+from proman import event_handler as handler
 
 if TYPE_CHECKING:
-    import argparse
+    from proman.manager import Manager
 
 
-def run(
-    path_repo_base: str | Path,
-    path_repo_head: str | Path,
-    token_github: str,
-    token_zenodo: str,
-    token_zenodo_sandbox: str,
-    github_context: _github_contexts.GitHubContext,
-):
-    logger.section("Execution")
+@logger.sectioner("Continuous Pipeline")
+def run(manager: Manager):
+    _set_git_api(manager=manager)
     event_to_handler = {
-        _EventType.ISSUES: _handler.IssuesEventHandler,
-        _EventType.ISSUE_COMMENT: _handler.IssueCommentEventHandler,
-        _EventType.PULL_REQUEST: _handler.PullRequestEventHandler,
-        _EventType.PULL_REQUEST_TARGET: _handler.PullRequestTargetEventHandler,
-        _EventType.PUSH: _handler.PushEventHandler,
-        _EventType.SCHEDULE: _handler.ScheduleEventHandler,
-        _EventType.WORKFLOW_DISPATCH: _handler.WorkflowDispatchEventHandler,
+        EventType.ISSUES: handler.IssuesEventHandler,
+        EventType.ISSUE_COMMENT: handler.IssueCommentEventHandler,
+        EventType.PULL_REQUEST: handler.PullRequestEventHandler,
+        EventType.PULL_REQUEST_TARGET: handler.PullRequestTargetEventHandler,
+        EventType.PUSH: handler.PushEventHandler,
+        EventType.SCHEDULE: handler.ScheduleEventHandler,
+        EventType.WORKFLOW_DISPATCH: handler.WorkflowDispatchEventHandler,
     }
-
-    reporter = Reporter(github_context=github_context)
-    output_writer = OutputManager()
-    handler_class = event_to_handler.get(github_context.event_name)
+    handler_class = event_to_handler.get(manager.gh_context.event_name)
     if handler_class:
         try:
-            handler_class(
-                github_context=github_context,
-                reporter=reporter,
-                output_writer=output_writer,
-                admin_token=token_github,
-                zenodo_token=token_zenodo,
-                zenodo_sandbox_token=token_zenodo_sandbox,
-                path_repo_base=path_repo_base,
-                path_repo_head=path_repo_head,
-            ).run()
-        except _exception.ProManException:
-            logger.section_end(target_level=current_log_section_level)
-            _finalize(
-                github_context=github_context,
-                reporter=reporter,
-                output_writer=output_writer,
-                repo_path=path_repo_head,
-            )
-            return
-        except Exception as e:
-            traceback = logger.traceback()
-            error_name = e.__class__.__name__
-            logger.critical(
-                f"Unexpected Error: {error_name}",
-                traceback,
-            )
-            reporter.update(
-                "main",
-                status="fail",
-                summary=f"An unexpected error occurred: `{error_name}`",
-                body=mdit.element.admonition(
-                    title=error_name,
-                    body=traceback,
-                    type="error",
-                    dropdown=True,
-                    opened=True,
-                ),
-            )
-            logger.section_end(target_level=current_log_section_level)
-            _finalize(
-                github_context=github_context,
-                reporter=reporter,
-                output_writer=output_writer,
-                repo_path=path_repo_head,
-            )
-            return
+            handler_class(manager=manager).run()
+        except Exception:
+            pass
     else:
         supported_events = mdit.inline_container(
             *(mdit.element.code_span(enum.value) for enum in event_to_handler),
@@ -98,92 +41,141 @@ def run(
         )
         event_description = mdit.inline_container(
             "Unsupported triggering event ",
-            mdit.element.code_span(github_context.event_name.value),
+            mdit.element.code_span(manager.gh_context.event_name.value),
         )
-        reporter.update_event_summary(event_description)
+        manager.reporter.update_event_summary(event_description)
         summary = mdit.inline_container(
             "Unsupported triggering event. Supported events are: ",
             supported_events,
         )
-        reporter.update("main", status="skip", summary=summary)
-    _finalize(
-        github_context=github_context,
-        reporter=reporter,
-        output_writer=output_writer,
-        repo_path=path_repo_head,
-    )
+        manager.reporter.update("main", status="skip", summary=summary)
+    _finalize(manager=manager)
     return
 
 
-def run_cli(args: argparse.Namespace):
-    """Run the CLI.
-
-    Parameters
-    ----------
-    args : argparse.Namespace, optional
-        The parsed arguments. If None, the arguments are parsed from sys.argv.
-    """
-    path_repo_base = _actionman.env_var.read(name="PATH_REPO_BASE", typ=str)
-    path_repo_head = _actionman.env_var.read(name="PATH_REPO_HEAD", typ=str)
-    admin_token = Token(
-        _actionman.env_var.read(name="GITHUB_ADMIN_TOKEN", typ=str), name="GitHub Admin"
-    )
-    zenodo_token = Token(_actionman.env_var.read(name="ZENODO_TOKEN", typ=str), name="Zenodo")
-    zenodo_sandbox_token = Token(
-        _actionman.env_var.read(name="ZENODO_SANDBOX_TOKEN", typ=str), name="Zenodo Sandbox"
-    )
-    github_context = _github_contexts.github.create(
-        context=_actionman.env_var.read(name="GITHUB_CONTEXT", typ=dict)
-    )
-
-    run(
-        path_repo_base=path_repo_base,
-        path_repo_head=path_repo_head,
-        token_github=admin_token,
-        token_zenodo=zenodo_token,
-        token_zenodo_sandbox=zenodo_sandbox_token,
-        github_context=github_context,
-    )
+def run_cli(args: dict) -> None:
+    """Run from CLI."""
+    run(manager=args["manager"])
     return
 
 
-@_logger.sectioner("Output Generation")
-def _finalize(
-    github_context: _github_contexts.GitHubContext,
-    reporter: Reporter,
-    output_writer: OutputManager,
-    repo_path: str,
-):
-    workflow_output, report_path = output_writer.generate(failed=reporter.failed)
+@logger.sectioner("Git API Setting")
+def _set_git_api(manager: Manager) -> None:
+    REPODYNAMICS_BOT_USER = (
+        "RepoDynamicsBot",
+        "146771514+RepoDynamicsBot@users.noreply.github.com",
+    )
+    # TODO: make authenticated
+    # Refs:
+    # - https://docs.github.com/en/authentication/managing-commit-signature-verification/signing-commits
+    # - https://docs.github.com/en/authentication/managing-commit-signature-verification/telling-git-about-your-signing-key
+    # - https://docs.github.com/en/authentication/managing-commit-signature-verification/adding-a-gpg-key-to-your-github-account
+    # - https://github.com/crazy-max/ghaction-import-gpg
+    # - https://stackoverflow.com/questions/61096521/how-to-use-gpg-key-in-github-actions
+    # - https://github.com/sigstore/gitsign
+    # - https://www.chainguard.dev/unchained/keyless-git-commit-signing-with-gitsign-and-github-actions
+    # - https://github.com/actions/runner/issues/667
+    # - https://sourceforge.net/projects/gpgosx/
+    # - https://www.gnupg.org/download/
+
+
+    apis = [("Target Repository", manager.git)]
+    if manager.main.git.repo_path != manager.git.repo_path:
+        apis.append(("Upstream Repository", manager.main.git))
+    for title, git_api in apis:
+        for user_type, name, email in (
+            ("author", manager.gh_context.event.sender.login, manager.gh_context.event.sender.github_email),
+            ("committer", *REPODYNAMICS_BOT_USER),
+        ):
+            git_api.set_user(
+                name=name,
+                email=email,
+                typ=user_type,
+                scope="process",
+            )
+            logger.info(
+                title,
+                f"{user_type.capitalize()} set to {name} <{email}>.",
+            )
+    return
+
+@logger.sectioner("GitHub API Verification")
+def _check_github_api(manager: Manager) -> None:
+    in_repo_creation_event = (
+            self.gh_context.event_name is _ghc_enum.EventType.PUSH
+            and self.gh_context.ref_type is _ghc_enum.RefType.BRANCH
+            and self.gh_context.event.action is _ghc_enum.ActionType.CREATED
+            and self.gh_context.ref_is_main
+        )
+    log_title = "Admin Token Verification"
+    if not admin_token:
+        has_admin_token = False
+        if in_repo_creation_event:
+            logger.info(
+                log_title,
+                "Repository creation event detected; no admin token required.",
+            )
+        elif self.gh_context.event.repository.fork:
+            logger.info(
+                log_title,
+                "Forked repository detected; no admin token required.",
+            )
+        else:
+            logger.critical(
+                log_title,
+                "No admin token provided.",
+            )
+            reporter.update(
+                "main",
+                status="fail",
+                summary="No admin token provided.",
+            )
+            raise ProManException()
+    else:
+        try:
+            check = api_admin.info
+        except _WebAPIError as e:
+            details = e.report.body
+            details.extend(*e.report.section["details"].content.body.elements())
+            logger.critical(
+                log_title, "Failed to verify the provided admin token.", details
+            )
+            reporter.update(
+                "main",
+                status="fail",
+                summary="Failed to verify the provided admin token.",
+                section=mdit.document(
+                    heading="Admin Token Verification",
+                    body=details.elements(),
+                ),
+            )
+            raise ProManException()
+        has_admin_token = True
+        logger.success(
+            log_title,
+            "Admin token verified successfully.",
+        )
+    return api_admin, api_actions, link_gen, has_admin_token
+
+
+@logger.sectioner("Output Generation")
+def _finalize(manager: Manager) -> None:
+    workflow_output = manager.output.generate()
     _write_step_outputs(workflow_output)
 
-    report_gha, report_full = reporter.generate()
+    report_gha = manager.reporter.generate(gha=True)
     _write_step_summary(report_gha)
 
-    log = logger.report
-    target_config, sphinx_output = make_sphinx_target_config()
-    log.target_configs["sphinx"] = target_config
-    log_html = log.render(target="sphinx")
-    logger.info(
-        "Log Generation Logs",
-        mdit.element.rich(Text.from_ansi(sphinx_output.getvalue())),
-    )
     filename = (
-        f"{github_context.repository_name}-workflow-run"
-        f"-{github_context.run_id}-{github_context.run_attempt}.{{}}.{{}}"
+        f"{manager.gh_context.repository_name}-workflow-run"
+        f"-{manager.gh_context.run_id}-{manager.gh_context.run_attempt}.{{}}.{{}}"
     )
-    dir_path = Path(repo_path) / report_path / "proman"
+    dir_path = manager.git.repo_path / manager.data["local"]["report"]["path"] / "proman" / "gha"
     dir_path.mkdir()
 
     output_str = ps.write.to_json_string(workflow_output, sort_keys=True, indent=3, default=str)
-
-    for file_data, file_suffix, file_ext in (
-        (output_str, "output", "json"),
-        (report_full, "report", "html"),
-        (log_html, "log", "html"),
-    ):
-        file = dir_path / filename.format(file_suffix, file_ext)
-        file.write_text(file_data)
+    file = dir_path / filename.format("output", "json")
+    file.write_text(output_str)
     return
 
 
@@ -191,7 +183,7 @@ def _write_step_outputs(kwargs: dict) -> None:
     log_outputs = []
     for name, value in kwargs.items():
         output_name = name.lower().replace("_", "-")
-        written_output = _actionman.step_output.write(name=output_name, value=value)
+        written_output = actionman.step_output.write(name=output_name, value=value)
         log_outputs.append(
             mdit.element.code_block(
                 written_output,
@@ -204,5 +196,5 @@ def _write_step_outputs(kwargs: dict) -> None:
 
 def _write_step_summary(content: str) -> None:
     logger.debug("GHA Summary Output", mdit.element.code_block(content))
-    _actionman.step_summary.write(content)
+    actionman.step_summary.write(content)
     return
