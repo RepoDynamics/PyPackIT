cb7e25d301b621d1a5f46d3cdaa2f6f718d5b621
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-13 13:13:14 +0200

    Update __init__.py

--- .manager/src/proman/__init__.py
+++ .manager/src/proman/__init__.py
@@ -1,3 +1,5 @@
 from proman import manager, report, script, token_manager, util
 
-__all__ = ["manager", "report", "script", "token_manager", "util"]
+__all__ = ["manager", "report", "script", "token_manager", "util", "__version__"]
+
+__version__: str = "1.0.0"
