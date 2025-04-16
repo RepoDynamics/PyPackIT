d22f040c6907a18e06af0cc41142a53a7452dd18
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-16 17:50:50 +0200

    Delete tasks_global.sh

--- .devcontainer/container-docs/tasks_global.sh
+++
@@ -1,7 +0,0 @@
-build() {
-    conda run --name website --live-stream -vv bash .devcontainer/container-docs/script/website/build.sh
-}
-
-live() {
-    conda run --name website --live-stream -vv bash .devcontainer/container-docs/script/website/livehtml.sh
-}
