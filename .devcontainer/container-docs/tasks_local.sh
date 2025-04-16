5444b1dcbb185f3ea1ff063e4785a807b875b6ed
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-16 17:50:53 +0200

    Delete tasks_local.sh

--- .devcontainer/container-docs/tasks_local.sh
+++
@@ -1,7 +0,0 @@
-build() {
-    conda run --cwd /workspace --name website --live-stream -vv bash .devcontainer/container-docs/script/website/build.sh
-}
-
-live() {
-    conda run --cwd /workspace --name website --live-stream -vv bash .devcontainer/container-docs/script/website/livehtml.sh
-}
