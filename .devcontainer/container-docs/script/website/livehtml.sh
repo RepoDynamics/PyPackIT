52b2e0b9cc430f9f738018576d6fb9950f8431f7
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-18 20:06:56 +0200

    Delete livehtml.sh

--- .devcontainer/container-docs/script/website/livehtml.sh
+++
@@ -1,5 +0,0 @@
-sphinx-autobuild \
-  docs/website/source \
-  docs/website/.build \
-  --verbose --show-traceback --keep-going --color --nitpicky --jobs auto \
-  -a -b=dirhtml --open-browser --delay 0
