a70f9b2386858e72d96cfac487af6c1e871c83f0
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-19 16:41:59 +0200

    remove apt-install feature

--- .devcontainer/features/apt_setup/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-RUN if [ -f /tmp/temp-env-files/bash.sh ]; then bash /tmp/temp-env-files/bash.sh; fi
-RUN apt-get update && export DEBIAN_FRONTEND=noninteractive && \
-  if [ -f /tmp/temp-env-files/apt.txt ]; then \
-    apt-get -y install --no-install-recommends $(xargs < /tmp/temp-env-files/apt.txt); \
-  fi && rm -rf /var/lib/apt/lists/*
