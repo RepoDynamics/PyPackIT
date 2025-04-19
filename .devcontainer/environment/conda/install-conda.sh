1404e890bbba718ef95d1348f3e8779ed61ed718
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-20 00:01:51 +0200

    Update install-conda.sh

--- .devcontainer/environment/conda/install-conda.sh
+++ .devcontainer/environment/conda/install-conda.sh
@@ -2,14 +2,16 @@
 
 set -euxo pipefail
 
+# Constants
 CONDA_ACTIVATION_SCRIPT_RELPATH="etc/profile.d/conda.sh"
 MAMBA_ACTIVATION_SCRIPT_RELPATH="etc/profile.d/mamba.sh"
 
+# Default arguments
 NAME="Miniforge3"
 VERSION="latest"
 CONDA_DIR="/opt/conda"
 GROUP="conda"
-USERNAME="${SUDO_USER:-$(id -un)}"
+USER="${SUDO_USER:-$(id -un)}"
 ACTIVATES=()
 NO_CLEAN=false
 NO_CACHE_CLEAN=false
@@ -17,17 +19,17 @@ INTERACTIVE=false
 REINSTALL=false
 INSTALLER_DIR="/tmp/miniforge"
 LOGFILE=""
+DEBUG=false
 
 show_help() {
 echo "============="
 echo "Install Conda"
 echo "============="
-echo "Usage: $0 <package-list-file> [OPTIONS]"
+echo "Usage: $0 [OPTIONS]"
 echo
 echo "Install conda and mamba using the Miniforge installer."
 echo
 echo "Options:"
-echo
 echo "    --name <name>           Name of the Miniforge variant to install."
 echo "                            Default: '$NAME'"
 echo "    --version               Version of the Miniforge variant to install."
@@ -38,10 +40,10 @@ echo "                            This corresponds to the CONDA_DIR environment 
 echo "                            Default: '$CONDA_DIR'"
 echo "    --group <name>          Name of a user group to give access to conda."
 echo "                            Default: '$GROUP'"
-echo "    --username <name>       Name of a user to add to the conda group."
+echo "    --user <name>             Name of a user to add to the conda group."
 echo "                            This user must already exist."
 echo "                            Defaults to the real user running this script."
-echo "                            Default: '$USERNAME'"
+echo "                            Default: '$USER'"
 echo "    --activate <path>       Path to a shell configuration file to append conda initialization to."
 echo "                            Can be provided multiple times."
 echo "                            Examples:"
@@ -58,6 +60,7 @@ echo "    --installer-dir <path>  Path to directory to download the Miniforge in
 echo "                            Default: '$INSTALLER_DIR'"
 echo "    --logfile <path>        Log all output (stdout + stderr) to this file in addition to console."
 echo "                            Otherwise output is only printed to the console."
+echo "    --debug                 Enable debug mode."
 echo "    --help                  Show this help message and exit."
 echo
 echo "Example:"
@@ -68,56 +71,45 @@ echo "    Miniforge Docker image: https://github.com/conda-forge/miniforge-image
 echo "    Miniforge repository README: https://github.com/conda-forge/miniforge?tab=readme-ov-file#install"
 }
 
-while [[ $# -gt 0 ]]; do
+OPTS=$(
+    getopt \
+        --longoptions name:,version:,conda-dir:,group:,user:,activate:,no-clean,no-cache-clean,interactive,reinstall,installer-dir:,logfile:,debug,help \
+        --name "$0" \
+        --options '' \
+        -- "$@"
+)
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to parse options." >&2
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+while true; do
     case "$1" in
-        --name)
-            shift
-            NAME=$1
-            ;;
-        --version)
-            shift
-            VERSION=$1
-            ;;
-        --conda-dir)
-            shift
-            CONDA_DIR=$1
-            ;;
-        --activate)
-            shift
-            ACTIVATES+=("$1")
-            ;;
-        --no-clean)
-            NO_CLEAN=true
-            ;;
-        --no-cache-clean)
-            NO_CACHE_CLEAN=true
-            ;;
-        --interactive)
-            INTERACTIVE=true
-            ;;
-        --reinstall)
-            REINSTALL=true
-            ;;
-        --installer-dir)
-            shift
-            INSTALLER_DIR=$1
-            ;;
-        --logfile)
-            shift
-            LOGFILE=$1
-            ;;
-        --help)
-            show_help
-            exit 0
-            ;;
-        *)
-            echo "Unknown option: $1" >&2
-            show_help >&2
-            exit 1
-            ;;
+        --name) NAME="$2"; shift 2;;
+        --version) VERSION="$2"; shift 2;;
+        --conda-dir) CONDA_DIR="$2"; shift 2;;
+        --group) GROUP="$2"; shift 2;;
+        --user) USER="$2"; shift 2;;
+        --activate) ACTIVATES+=("$2"); shift 2;;
+        --no-clean) NO_CLEAN=true; shift;;
+        --no-cache-clean) NO_CACHE_CLEAN=true; shift;;
+        --interactive) INTERACTIVE=true; shift;;
+        --reinstall) REINSTALL=true; shift;;
+        --installer-dir) INSTALLER_DIR="$2"; shift 2;;
+        --logfile) LOGFILE="$2"; shift 2;;
+        --debug) DEBUG=true; shift;;
+        --help) show_help; exit 0;;
+        --) shift; break;;
+        *) echo "Unknown option: $1" >&2; show_help >&2; exit 1;;
     esac
-    shift
 done
+
+if ! [[ "$DEBUG" == true ]]; then
+    set +x
+fi
 
 if [[ -n "$LOGFILE" ]]; then
     echo "📝 Initializing logging to '$LOGFILE'."
@@ -141,7 +133,7 @@ if command -v conda >/dev/null 2>&1; then
         rm -rf "$(conda info --base)"
         rm -f "$HOME/.condarc"
         rm -rf "$HOME/.conda"
-        user_home=$(getent passwd "$USERNAME" | cut -d: -f6)
+        user_home=$(getent passwd "$USER" | cut -d: -f6)
         rm -rf "$user_home/.condarc"
         rm -rf "$user_home/.conda"
     else
@@ -247,10 +239,9 @@ fi
 
 # Add group and fix permissions
 # - https://github.com/devcontainers/features/blob/8895eb3d161d28ada3a8de761a83135e811cae3d/src/conda/install.sh#L81-L115
-
 getent group "$GROUP" >/dev/null || groupadd -r "$GROUP"
-id -nG "$USERNAME" | grep -qw "$GROUP" || usermod -a -G "$GROUP" "$USERNAME"
-chown -R "$USERNAME:$GROUP" "$CONDA_DIR"
+id -nG "$USER" | grep -qw "$GROUP" || usermod -a -G "$GROUP" "$USER"
+chown -R "$USER:$GROUP" "$CONDA_DIR"
 chmod -R g+r+w "$CONDA_DIR"
 find "$CONDA_DIR" -type d -print0 | xargs -n 1 -0 chmod g+s
 
