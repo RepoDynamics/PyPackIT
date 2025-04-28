f7858b5861d0ec58db2f29e86c470e245d32e676
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-04-28 21:02:13 +0200

    Update tasks.sh

--- .devcontainer/tasks.sh
+++ .devcontainer/tasks.sh
@@ -1,22 +1,22 @@
 project() {
-    conda run --cwd /workspace --live-stream -vv --name website proman "$@"
+    conda run --cwd /pypackit --live-stream -vv --name proman proman "$@"
 }
 
 build-oj-paper() {
     current_dir=$(pwd)
-    cd '/workspace'
-    docker exec -it openjournals \
+    cd '/pypackit'
+    docker exec -it openjournals-inara \
       inara -o pdf -p -v $1
     cd "$current_dir"
 }
 
 typecheck() {
-    conda run --cwd /workspace --name type_check --live-stream -vv mypy '--package pypackit' '--package pypackit_testsuite' '--python-executable /opt/conda/envs/app/bin/python' '--config-file .config/mypy.toml' --install-types --non-interactive
+    conda run --cwd /pypackit --name type_check --live-stream -vv mypy '--package pypackit' '--package pypackit_testsuite' '--python-executable /opt/conda/envs/app/bin/python' '--config-file .config/mypy.toml' --install-types --non-interactive
 }
 
 jupyterlab() {
     current_dir=$(pwd)
-    cd '/workspace'
+    cd '/pypackit'
     conda activate 'jupyter'
     jupyter-lab / --ContentsManager.allow_hidden=True --IdentityProvider.token=''
     conda deactivate
@@ -24,9 +24,9 @@ jupyterlab() {
 }
 
 build() {
-    conda run --cwd /workspace --name website --live-stream -vv bash .devcontainer/script/website_build.sh
+    conda run --cwd /pypackit --name website --live-stream -vv bash .devcontainer/script/website_build.sh
 }
 
 live() {
-    conda run --cwd /workspace --name website --live-stream -vv bash .devcontainer/script/website_livehtml.sh
+    conda run --cwd /pypackit --name website --live-stream -vv bash .devcontainer/script/website_build.sh bash .devcontainer/script/website_livehtml.sh
 }
